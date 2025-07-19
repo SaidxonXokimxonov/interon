6823914ae14abd135c1b98934ce36be247845e48
SaidxonXokimxonov <saidxonxokimxonov006@gmail.com>
2025-07-19 21:44:31 +0500

    rebuild

--- src/pages/user/comonents/avatar.tsx
+++ src/pages/user/comonents/avatar.tsx
@@ -47,7 +47,7 @@ const ProfileMenu = () => {
       {isOpen && (
         <div className="absolute right-0 mt-2 w-44 bg-white border border-gray-200 rounded-lg shadow-lg z-20">
           <NavLink
-            to="/user"
+            to="/user/profile"
             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
           >
             Profile
