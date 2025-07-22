1bf2315d25bf6e7e9e6bcb06486281f45f83ff3f
SaidxonXokimxonov <saidxonxokimxonov006@gmail.com>
2025-07-22 13:24:04 +0500

    'rebuild'

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -14,6 +14,8 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const auth = getAuth(app);
